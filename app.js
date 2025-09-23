7499ece109ee7cd73db3c5e583356df373060913
onzk0120 <222937425+onzk0120@users.noreply.github.com>
2025-09-23 20:38:45 +0900

    条件分岐

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-// 足し算
-console.log(5 + 3);
+let score = 80;
 
-// 引き算
-console.log(7 - 5);
-
-// 掛け算
-console.log(4 * 3);
-
-// 割り算
-console.log(8 / 2);
-
-// 余り
-console.log(10 % 3);
+if(score >= 90){
+  alert('優秀');
+} else if(score >= 60){
+  alert('合格');
+} else{
+  alert('再試験');
+}
