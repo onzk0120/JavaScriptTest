cc0fc76c2f8502e87585770db93306eee6d19ee0
onzk0120 <222937425+onzk0120@users.noreply.github.com>
2025-09-27 22:15:34 +0900

    FizzBuzz

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-let myname = 'John';
+let input = prompt('数字を入力してください');
 
-let hello = greet(myname);
+fizzBuzz(input);
 
-console.log(hello);
-
-function greet(name){
-    let greeting = 'Hello,' + name;
-    return greeting;
+function fizzBuzz(num) {
+    if (num % 15 === 0) {
+        alert('FizzBuzz');
+    } else if (num % 3 === 0) {
+        alert('Fizz');
+    } else if (num % 5 === 0) {
+        alert('Buzz');
+    } else {
+        alert(num);
+    }
 }
