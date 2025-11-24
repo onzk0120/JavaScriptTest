088933caefd264ee4310bfe3a3903b0ff1c73432
onzk0120 <222937425+onzk0120@users.noreply.github.com>
2025-11-24 21:56:42 +0900

    1124

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-const myname = "taro"
-console.log(myname)
-myname = "jiro"
-console.log(myname)
+var str = "グローバルスコープ"
+
+function fun() {
+  console.log(str)
+  var y = "関数スコープ"
+}
+
+fun()
+console.log(y)
