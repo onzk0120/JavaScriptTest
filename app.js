9c747caf942884965f909feb7cf629b4a78883ff
onzk0120 <222937425+onzk0120@users.noreply.github.com>
2025-12-05 23:52:19 +0900

    1205

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-var str = "グローバルスコープ"
-
-function fun() { 
-  var str
-  console.log(str)
-  var str = "関数スコープ"
-  console.log(str)
+function greet(name){
+    let greeting = 'Hello,' + name;
+    return greeting;
 }
-
-fun()
