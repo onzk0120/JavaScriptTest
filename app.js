844fa6b924f4fdc4c98cccca328061fff0a50ea5
onzk0120 <222937425+onzk0120@users.noreply.github.com>
2025-12-08 23:17:50 +0900

    1208

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-let promptStr = prompt('数字を入力してください');
+let input = prompt('数字を入力してください');
+
+fizzBuzz(input);
+
+function fizzBuzz(num) {
+    if (num % 15 === 0) {
+        alert('FizzBuzz');
+    } else if (num % 3 === 0) {
+        alert('Fizz');
+    } else if (num % 5 === 0) {
+        alert('Buzz');
+    } else {
+        alert(num);
+    }
+}
